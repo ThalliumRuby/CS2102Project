0410c73a16d0aa1a586903a1282bde83f6dbafea
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2021-10-16 00:22:26 +0800

    updated assigned schemas

---
+++ schema-draft.sql
@@ -0,0 +1,20 @@
+CREATE TABLE healthDeclaration(
+    date DATE,
+    temp NUMERIC(3, 1) NOT NULL,
+    fever BOOLEAN NOT NULL,
+    eid INTEGER,
+    FOREIGN KEY (eid) REFERENCES Employees(eid),
+    PRIMARY KEY (eid, date),
+);
+
+CREATE TABLE Employees(
+    eid INTEGER,
+    ename VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+    contact VARCHAR(50) NOT NULL,
+    resignedDate DATE,
+    type VARCHAR(10),
+    PRIMARY KEY (eid),
+    CHECK (type IN {'Junior', 'Senior', 'Manager'})
+);
+
