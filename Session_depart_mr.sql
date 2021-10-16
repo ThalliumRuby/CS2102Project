aa859217214157befb3cc2bae752d2b69cf8da9e
lury-18 <90122114+lury-18@users.noreply.github.com>
2021-10-16 11:12:43 +0800

    Add files via upload

---
+++ Session_depart_mr.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Department(
+    did INTEGER,
+    dname VARCHAR(50) NOT NULL,
+    PRIMARY KEY (did)
+);
+
+CREATE TABLE MeetingRooms(
+    floor INTEGER,
+    room INTEGER,
+    rname VARCHAR(50) NOT NULL,
+    capacity INTEGER,
+    update_date DATE,
+    PRIMARY KEY (floor, room)
+);
+
+CREATE TABLE Sessions(
+    session_date DATE,
+    session_time TIME,
+    PRIMARY KEY (session_date,session_time),
+);
