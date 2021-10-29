42249a0cfef738b330f3390d947860477914ff8e
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2021-10-30 01:18:33 +0800

    update schema tables:meeting rooms, sessions, updates

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS healthDeclaration, Employees, Departments, MeetingRooms, Sessions, Updates;
 
+DROP VIEW IF EXISTS Junior, Senior, Manager;
+
 CREATE TABLE healthDeclaration(
     declareDate DATE,
     temp NUMERIC(3, 1) NOT NULL,
@@ -34,7 +36,9 @@ CREATE TABLE MeetingRooms(
     rname VARCHAR(50) NOT NULL,
     capacity INTEGER,
     update_date DATE,
-    PRIMARY KEY (floors, room)
+    did INTEGER ,
+    PRIMARY KEY (floors, room),
+    FOREIGN KEY (did) REFERENCES Departments(did)
 );
 
 CREATE TABLE Sessions(
@@ -42,8 +46,13 @@ CREATE TABLE Sessions(
     session_time TIME,
     session_floor INTEGER,
     session_room INTEGER,
-    PRIMARY KEY (session_date,session_time, session_floor, session_room)
-    FOREIGN KEY(session_floor, session_room) REFERENCES MeetingRooms(floors, room)
+    participant_id INTEGER ,
+    booker_id INTEGER NOT NULL,
+    is_approved BOOLEAN DEFAULT NULL,
+    PRIMARY KEY (session_date,session_time, session_floor, session_room, participant_id),
+    FOREIGN KEY(session_floor, session_room) REFERENCES MeetingRooms(floors, room),
+    FOREIGN KEY(participant_id) REFERENCES Employees(eid),
+    FOREIGN KEY(booker_id) REFERENCES Employees(eid)
 );
 
 CREATE TABLE Updates(
@@ -51,6 +60,35 @@ CREATE TABLE Updates(
     new_cap INTEGER ,
     floors INTEGER ,
     room INTEGER ,
+    eid INTEGER NOT NULL,
     PRIMARY KEY (date),
-    FOREIGN KEY (floors, room) REFERENCES MeetingRooms(floors, room)
+    FOREIGN KEY (floors, room) REFERENCES MeetingRooms(floors, room),
+    FOREIGN KEY(eid) REFERENCES Employees(eid)
+);
+
+CREATE OR REPLACE VIEW Junior AS(
+    SELECT
+                     eid, did
+              FROM
+                     Employees
+              WHERE
+                     ekind = 'Junior'
+);
+
+CREATE OR REPLACE VIEW Senior AS(
+    SELECT
+                     eid, did
+              FROM
+                     Employees
+              WHERE
+                     ekind = 'Senior'
+);
+
+CREATE OR REPLACE VIEW Manager AS(
+    SELECT
+                     eid, did
+              FROM
+                     Employees
+              WHERE
+                     ekind = 'Manager'
 );
