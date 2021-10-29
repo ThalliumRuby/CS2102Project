0e88d56df259a97e3aff9bf4d72e6fd5dcb90839
ThalliumRuby <69462056+ThalliumRuby@users.noreply.github.com>
2021-10-29 17:36:11 +0800

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -16,3 +16,25 @@ INSERT INTO meetingrooms
        (floor_no, room_no, room_name, capacity)
 $$ LANGUAGE sql ;
 
+CREATE OR REPLACE PROCEDURE change_capacity(IN floor_no INTEGER, IN room_no INTEGER, IN new_capacity INTEGER, IN change_date DATE)
+AS $$
+
+	UPDATE MeetingRooms
+	SET capacity = capacity + new_capacity, update_date = change_date
+	WHERE floor_no = floors AND room_no = room;
+
+$$ LANGUAGE sql;
+
+CREATE OR REPLACE PROCEDURE add_employee(IN employee_name VARCHAR(50), IN contact_num INTEGER, IN kind VARCHAR(10), IN department_id INTEGER)
+AS $$
+	INSERT INTO Employees (eid, did, ename, contact, ekind)
+		VALUES(max(eid)+1, department_id, employee_name, contact_num, kind);
+		
+$$ LANGUAGE sql;
+
+CREATE OR REPLACE PROCEDURE remove_employee(IN employee_id INTEGER, IN retired_date DATE)
+AS $$
+	UPDATE Employees
+	SET resignedDate = retired_date;
+
+$$ LANGUAGE sql;
